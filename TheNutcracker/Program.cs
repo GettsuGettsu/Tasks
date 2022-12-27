ed0d69d287941daba06d3f1ff381b191b0045e4b
GettsuGettsu <gettsuga.tenso@gmail.com>
2022-12-27 23:25:24 +0300

    start building the Nutcracker

--- TheNutcracker/Program.cs
+++ TheNutcracker/Program.cs
@@ -7,4 +7,44 @@
             Console.WriteLine("Hello, World!");
         }
     }
+
+    public class Water
+    {
+
+    }
+
+    public class Dophin
+    {
+        private readonly string skin = "Golden";
+    }
+
+    public class Sea : Water
+    {
+
+    }
+
+    public class Splashes : Water
+    {
+
+    }
+
+    public class Arapchonok
+    {
+
+    }
+
+    public class Umbrella
+    {
+
+    }
+
+    public class Maria
+    {
+
+    }
+
+    public class Nutcraker
+    {
+
+    }
 }
